074c00ca7a07b254073c4319956034e4cca724f6
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2022-11-24 10:36:35 +0000

    Added extra comments on assignment requirements

--- index.js
+++ index.js
@@ -108,3 +108,25 @@ console.log(typeof profitloss);
 // Calculate net profit and loss.  Need to deconstruct array.  Loop over profitloss part of array and add numbers together.  Total showing as NaN.  Need to research whether the negative numbers are affecting the calculation and maybe reformat the profitloss variable.
 
 // let netProfitLoss = Maths.sum(finances);
+
+// The average of the **changes** in Profit/Losses over the entire period.
+// * You will need to track what the total change in profits are from month to month and then find the average.
+// * (`Total/Number of months`)
+
+// * The greatest increase in profits (date and amount) over the entire period.
+
+// * The greatest decrease in losses (date and amount) over the entire period.
+
+// When you open your code in the browser your resulting analysis should look similar to the following:
+
+// ```text
+// Financial Analysis
+// ----------------------------
+// Total Months: 25
+// Total: $2561231
+// Average  Change: $-2315.12
+// Greatest Increase in Profits: Feb-2012 ($1926159)
+// Greatest Decrease in Profits: Sep-2013 ($-2196167)
+// ```
+
+// Your final code should print the analysis to the console.
